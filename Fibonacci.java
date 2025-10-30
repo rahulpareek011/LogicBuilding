e8e9a2bb99976f430b4af4a6c7d27079e8f14dce
rahul pareek <itsrahul615@gmail.com>
2025-10-30 22:38:42 +0530

    Printing Fibonacci number in java

--- Fibonacci.java
+++ Fibonacci.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 
 public class Fibonacci{
     public static void main(String[] args) {
+        //default starting value
         int num1 = 0;
         int num2 = 1;
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+        //always printing value
         System.out.print(num1+" "+num2);
 
+        //loop from 2
         for(int i=2;i<n;i++){
-            int num3 = num1+num2;
-            System.out.print(" "+num3);
+            int num3 = num1+num2;//num 3 for next number
+            System.out.print(" "+num3); //printing fibonacci
 
-            num1 = num2;
-            num2 = num3;
+            num1 = num2;//num 2 going to num 1
+            num2 = num3;//num 3 going to num2
         }
     }
 }
